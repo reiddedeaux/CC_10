1615b44f3c0e3447fc6aa4a741bdbfbe2a84fa11
reiddedeaux <rdedeaux@usf.edu>
2025-02-23 21:54:40 -0500

    Task 2 - Created Order Class

--- CC_10.js
+++ CC_10.js
@@ -18,3 +18,18 @@ console.log(prod1.getDetails());
 
 prod1.updateStock(3);
 console.log(prod1.getDetails()); // Product: Laptop, ID: 101, Price: 1200, Stock: 7
+// Task 2 Created Order Class
+class Order {
+    constructor(orderId, product, quantity){
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        this.product.updateStock(quantity);
+    }
+    getOrderDetails(){
+        return `Order ID: ${this.orderID}, Product: ${this.product}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}`;
+    };
+};
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails());
+console.log(prod1.getDetails());
